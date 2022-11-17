bc88115f2b1cce710a3ce8c92a409b7b5e96651e
María Alejandra Ramírez <ramirez@evolbio.mpg.de>
2022-11-17 17:43:30 +0100

    Add shell script to create folder structure for data and plots

---
+++ FolderStructure.sh
@@ -0,0 +1,17 @@
+"""
+Creates folder structures to store data and plots
+"""
+
+#data folders
+mkdir -p data/Deterministic
+mkdir -p data/Stochastic
+mkdir -p data/Quantifiers/QuantifiersDet
+mkdir -p data/Quantifiers/QuantifiersSto
+
+#plots folders
+mkdir -p plots/Deterministic/FractalDimDet
+mkdir -p plots/Deterministic/TernaryDet
+mkdir -p plots/Deterministic/TimeseriesDet
+mkdir -p plots/Stochastic/FractalDimSto
+mkdir -p plots/Stochastic/TernarySto
+mkdir -p plots/Stochastic/TimeseriesSto
