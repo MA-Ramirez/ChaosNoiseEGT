a4f7c7f4b902c26ed4e3dab7dadd4de832af4da0
María Alejandra Ramírez <ramirez@evolbio.mpg.de>
2023-07-31 18:20:06 +0200

    New parameters for BN interpretation

--- cluster_scripts/ParametersToRun.jl
+++ cluster_scripts/ParametersToRun.jl
@@ -4,12 +4,13 @@ Script used to determine the parameters for which the stochastic simulations wil
 
 using DelimitedFiles
 
-B = [0,0.005,0.01,0.05,0.1,0.5,1.0,10.0]
+BN = [0,50.0,75.0,100.0,250.0,500.0,750.0,1000.0]
 N = [100,250,500,750,1000,2500,5000,7500,10000,25000,50000]
 
-for i in B
+for i in BN
     for j in N
-        params = Any[i,j]
+        beta = i/j
+        params = Any[beta,j]
         #The parameters are saved in cluster_scripts
         open("cluster_scripts/Parameters.txt", "a") do io
             writedlm(io, adjoint(params), " ")
