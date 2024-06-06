9787efe44092f144179a12941b5a776f224e33ef
María Alejandra Ramírez <ramirez@evolbio.mpg.de>
2024-06-06 12:12:40 +0200

    Update stochastic approach

--- src/StochasticSRC.jl
+++ src/StochasticSRC.jl
@@ -11,13 +11,28 @@ include(srcdir("PayoffMatrix.jl"))
 #                      PAIRWISE COMPARISON PROCESS functions                   #
 ################################################################################
 
-"""
+""" 
     picktype_randomly(num_types) → Int64
 Randomly picks a type among the different types present in the population
 Generates and returns a random number from the indexable collection `1:num_types`
 """
-function pick_type_randomly(num_types)
-    random_type = rand(1:num_types)
+function pick_type_randomly(amounts)
+    #Random number used to randomly pick types
+    randomnumber_decision = rand()
+
+    limit_1 = amounts[1]
+    limit_2 = amounts[1] + amounts[2]
+    limit_3 = amounts[1] + amounts[2] + amounts[3]
+
+    if randomnumber_decision <= limit_1
+        random_type = 1
+    elseif randomnumber_decision > limit_1 && randomnumber_decision <= limit_2
+        random_type = 2
+    elseif randomnumber_decision > limit_2 && randomnumber_decision <= limit_3
+        random_type = 3
+    else
+        random_type = 4
+    end
     return random_type
 end
 
@@ -54,14 +69,16 @@ end
 `probability`: probability for i to replace j
 Updates the `amounts` vector after one replacement decision
 """
-function replacement_decision_update(Player_i,Player_j,amounts,probability)
+function replacement_decision_update(Player_i,Player_j,amounts,probability,populationsize)
+    step_size = 1.0/populationsize
+    
     #Random number used to take the replacement decision
     randomnumber_decision = rand()
 
-    if randomnumber_decision < probability
+    if randomnumber_decision <= probability
         #i replaces j
-        amounts[Player_i] += 1
-        amounts[Player_j] -= 1
+        amounts[Player_i] += step_size
+        amounts[Player_j] -= step_size
     end
     return amounts
 end
@@ -72,11 +89,10 @@ end
 `B`: selection intensity coefficient
 Generates a step in the process, returns the updated `amounts` vector
 """
-function step(amounts,B)
-    num_types = size(payoff_matrix)[1]
+function step(amounts,B,populationsize)
 
-    Player_i = pick_type_randomly(num_types)
-    Player_j = pick_type_randomly(num_types)
+    Player_i = pick_type_randomly(amounts)
+    Player_j = pick_type_randomly(amounts)
 
     #If the two randomly chosen individuals are of the same type,
     # there is no need for replacement
@@ -86,11 +102,10 @@ function step(amounts,B)
         fi = fit_vals[Player_i]
         fj = fit_vals[Player_j]
         probability = replacement_probability(fi,fj,B)
-        amounts_update = replacement_decision_update(Player_i,Player_j,amounts,probability)
+        amounts_update = replacement_decision_update(Player_i,Player_j,amounts,probability,populationsize)
     else
         amounts_update = amounts
     end
-
     return amounts_update
 end
 
@@ -103,10 +118,10 @@ end
 If any of the quantities in `amounts` reaches the boundary, it returns true.
 i.e. when any quantity in `amounts` reaches zero (the process should be stopped)
 """
-function reach_boundary(amounts)
+function reach_boundary(amounts,populationsize)
     stop = false
     for i in amounts
-        if i == 0.0
+        if i < (1/populationsize)
             stop = true
         end
     end
@@ -119,65 +134,33 @@ end
 It runs the pairwise comparison process step for `time_steps` amount of times.
 It returns an array of arrays with the data of the evolution of the process
 """
-function run_process(initial_amounts, Beta, time_steps)
+function run_full_simulation(initial_condition, Beta, populationsize, time_steps)
     data_process = []
 
     #Initialise first entry with initial conditions
-    push!(data_process,initial_amounts)
+    push!(data_process,initial_condition)
 
     for i in 2:time_steps
         #Deep copy to copy by value not by reference
         prev_step_data = deepcopy(data_process[end])
-        step_data = step(prev_step_data,Beta)
+        step_data = step(prev_step_data,Beta,populationsize)
 
         #If any type reaches the boundary, stop the process
-        if reach_boundary(step_data) == true
+        if reach_boundary(step_data,populationsize) == true
             push!(data_process,step_data)
             break
         end
-
         push!(data_process,step_data)
     end
     return data_process
 end
 
 """
-    normalise_data(data_trajectory, populationsize) → Vector{Vector{Float64}}
-It normalises the data of the evolution of the process.
-The quantities are now given in relative amounts
-"""
-function normalise_data(data_trajectory, populationsize)
-    size_data_trajectory = size(data_trajectory)[1]
-    #Initialise empty array
-    normalised_data = fill(Float64[], size_data_trajectory)
-
-    for i in 1:size_data_trajectory
-        normalised_data[i] = data_trajectory[i]/populationsize
-    end
-
-    return normalised_data
-end
-
-"""
-    run_full_simulation(initial_amounts, Beta, populationsize, time_steps) → Vector{Vector{Float64}}
-`Beta`: selection intensity coefficient
-It runs the pairwise comparison process step for `time_steps` amount of times.
-It returns an array of arrays with the data of the evolution of the process.
-The data is normalised, so the quantities are given in relative amounts.
-"""
-function run_full_simulation(initial_amounts, Beta, populationsize, time_steps)
-    data_process = run_process(initial_amounts, Beta, time_steps)
-    normalised_data = normalise_data(data_process, populationsize)
-
-    return normalised_data
-end
-
-"""
     full_simulation(initial_amounts, Beta, populationsize, time_steps) → Matrix{Float64}
 Returns the results of `run_full_simulation` for easier data handling in the notebook
 """
-function full_simulation(initial_amounts, Beta, populationsize, time_steps)
-    data_vectors = run_full_simulation(initial_amounts, Beta, populationsize, time_steps)
+function full_simulation(initial_condition, Beta, populationsize, time_steps)
+    data_vectors = run_full_simulation(initial_condition, Beta, populationsize, time_steps)
     data_matrix = reduce(vcat,transpose.(data_vectors))
     return data_matrix
 end
@@ -185,21 +168,6 @@ end
 ################################################################################
 #                          SET PARAMETERS functions                            #
 ################################################################################
-
-"""
-    set_initial_conditions(populationsize) → Vector{Float64}
-The Skyrms' attractor can be properly seen starting with initial conditions
-close to the center of the simplex
-"""
-function set_initial_conditions(populationsize)
-    scaling_factor = 0.25
-    #Amount of individuals is an integer
-    A_ini = round(populationsize*scaling_factor)
-    B_ini = A_ini
-    C_ini = A_ini
-    D_ini = A_ini
-    return [A_ini,B_ini,C_ini,D_ini]
-end
 
 """
     set_timesteps(populationsize) → Int64
@@ -211,5 +179,11 @@ the attractor and for the running time of the quantifiers doesn't explode
 function set_timesteps(populationsize)
     constant_factor = 0.1
     tau = Int(populationsize/constant_factor)
+    return tau
+end
+
+function set_timesteps_FD(populationsize)
+    constant_factor = 500.0/3.0
+    tau = round(Int, populationsize*constant_factor)
     return tau
 end
